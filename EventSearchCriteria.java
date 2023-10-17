86d166dc53ad26397f879c5b50c4c767f6895d4b
iuwz <126331442+iuwz@users.noreply.github.com>
2023-10-17 19:00:50 +0300

    Update EventSearchCriteria.java

--- EventSearchCriteria.java
+++ EventSearchCriteria.java
@@ -1,21 +1,21 @@
 public class EventSearchCriteria implements LinkedListADT.SearchCriteria<Event> {
-	private String criteria;
-    private int type;
+	private String criteria;//1
+    private int type;//1
 
     
     public EventSearchCriteria(String criteria, int type) {
-    	this.criteria = criteria;
-        this.type = type;
+    	this.criteria = criteria;//1
+        this.type = type;//1
     }
 
     @Override
     public boolean matches(Event data) {
         switch (type) {
-            case 1://by contact name
-                return data.getContact().getName().equalsIgnoreCase(criteria);
-            case 2://by event title
-                return data.getTitle().equalsIgnoreCase(criteria);
+            case 1://1 //by contact name
+                return data.getContact().getName().equalsIgnoreCase(criteria);//1
+            case 2://1 //by event title
+                return data.getTitle().equalsIgnoreCase(criteria);//1
         }
-        return false;
+        return false;//1
     }
 }
