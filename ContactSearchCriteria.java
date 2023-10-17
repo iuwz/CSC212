7d44bd322dca50e883d396cb3134e2f8f0977a04
mazenfkh <145606447+mazenfkh@users.noreply.github.com>
2023-10-17 23:47:37 +0300

    Update ContactSearchCriteria.java

--- ContactSearchCriteria.java
+++ ContactSearchCriteria.java
@@ -1,7 +1,14 @@
-/**
- * This class implements the SearchCriteria interface and is used to search for a contact
- * This class allows us to search for a contact by name, phone number, email address, address or birth date
- */
+/*************Example***************
+CLASS: ContactSearchCriteria..java
+CSC212 Data structures - Project phase I
+Fall 2023
+EDIT DATE:
+7-9 Oct
+TEAM:
+Code Crafters 
+AUTHORS: Mazen Alkhodairi, Abdulaziz Alali, Abdulaziz Altekhaifi
+authors-names Alkhodairi 443102007 , Alali (443100240), Altekhaifi 443100756
+***********************************/
 public class ContactSearchCriteria implements LinkedListADT.SearchCriteria<Contact> {
     private String criteria;
     private int type;
