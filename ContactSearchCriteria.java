12f645c08f6e1992729055b52268a6903e3b503f
iuwz <126331442+iuwz@users.noreply.github.com>
2023-10-17 18:59:55 +0300

    Update ContactSearchCriteria.java

--- ContactSearchCriteria.java
+++ ContactSearchCriteria.java
@@ -3,30 +3,29 @@
  * This class allows us to search for a contact by name, phone number, email address, address or birth date
  */
 public class ContactSearchCriteria implements LinkedListADT.SearchCriteria<Contact> {
-    private String criteria;
-    private int type;
+    private String criteria;//1
+    private int type;//1
 
     public ContactSearchCriteria(String criteria, int type) {
        
-        this.criteria = criteria;
-        this.type = type;
+        this.criteria = criteria;//1
+        this.type = type;//1
     }
 
-   
     public boolean matches(Contact contact) {
         switch (type) {
-            case 1:  // Name
-                return criteria.equalsIgnoreCase(contact.getName());
-            case 2:  // Phone Number
-                return criteria.equalsIgnoreCase(contact.getPhoneNumber());
-            case 3:  // Email Address
-                return criteria.equalsIgnoreCase(contact.getEmailAddress());
-            case 4:  // Address
-                return criteria.equalsIgnoreCase(contact.getAddress());
-            case 5:  // Birth Date
-                return criteria.equalsIgnoreCase(contact.getBirthDate());
-            default:
-                return false;
+            case 1: //1 // Name
+                return criteria.equalsIgnoreCase(contact.getName());//1
+            case 2://1  // Phone Number
+                return criteria.equalsIgnoreCase(contact.getPhoneNumber());//1
+            case 3://1  // Email Address
+                return criteria.equalsIgnoreCase(contact.getEmailAddress());//1
+            case 4://1  // Address
+                return criteria.equalsIgnoreCase(contact.getAddress());//1
+            case 5://1  // Birth Date
+                return criteria.equalsIgnoreCase(contact.getBirthDate());//1
+            default://1
+                return false;//1
         }
     }
 
