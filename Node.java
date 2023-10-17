7e8b9a568ffa8533a05a32dd181dbda2b42c44e1
mazenfkh <145606447+mazenfkh@users.noreply.github.com>
2023-10-17 18:53:39 +0300

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,25 +1,25 @@
 public class Node<T> {
-    private T data;
-    private Node<T> next;
+    private T data; //1
+    private Node<T> next; //1
 
     public Node(T data) {
-        this.data = data;
-        this.next = null;
+        this.data = data; //1
+        this.next = null; //1
     }
 
     public T getData() {
-        return data;
+        return data; //1
     }
 
     public Node<T> getNext() {
-        return next;
+        return next; //1
     }
 
     public void setData(T data) {
-        this.data = data;
+        this.data = data; //1
     }
 
     public void setNext(Node<T> next) {
-        this.next = next;
-    }
+        this.next = next; //1
+    } 
 }
